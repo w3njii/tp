039c4d1af356230a674354b9e1ee1f8c43ab3d37
w3njii <wenjie.chong2018@gmail.com>
2025-10-02 22:38:54 +0800

    Fix checkstyle violations

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -18,9 +18,16 @@ import seedu.address.model.person.Remark;
 public class RemarkCommand extends Command {
 
     public static final String COMMAND_WORD = "remark";
-
     public static final String MESSAGE_ADD_REMARK_SUCCESS = "Added remark to Person: %1$s";
     public static final String MESSAGE_DELETE_REMARK_SUCCESS = "Removed remark from Person: %1$s";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Edits the remark of the person identified "
+            + "by the index number used in the last person listing. "
+            + "Existing remark will be overwritten by the input.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "r/ [REMARK]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + "r/ Likes to swim.";
 
     private final Index index;
     private final Remark remark;
@@ -35,15 +42,6 @@ public class RemarkCommand extends Command {
         this.index = index;
         this.remark = remark;
     }
-
-    public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Edits the remark of the person identified "
-            + "by the index number used in the last person listing. "
-            + "Existing remark will be overwritten by the input.\n"
-            + "Parameters: INDEX (must be a positive integer) "
-            + "r/ [REMARK]\n"
-            + "Example: " + COMMAND_WORD + " 1 "
-            + "r/ Likes to swim.";
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,10 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructs a Remark.
+     * @param remark The remark string.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
