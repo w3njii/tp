6b98eb66383471c2701733897be6aad9e07bb5ba
camlin-tan <187715771+camlin-tan@users.noreply.github.com>
2025-10-08 15:51:33 +0800

    Update all test cases

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -30,7 +30,7 @@ public class Person {
      * Every field must be present and not null.
      */
     public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags, BloodType bloodType) {
-        requireAllNonNull(name, phone, email, address, tags);
+        requireAllNonNull(name, phone, email, address, tags, bloodType);
         this.name = name;
         this.phone = phone;
         this.email = email;
