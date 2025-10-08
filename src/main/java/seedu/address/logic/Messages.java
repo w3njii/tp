4eeca110a9848a760a4272f3eb32a48f1fabda86
SoyWater <ivanlxy123@gmail.com>
2025-10-08 16:57:15 +0800

    Add date of birth to printed message

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -43,6 +43,8 @@ public class Messages {
                 .append(person.getEmail())
                 .append("; Address: ")
                 .append(person.getAddress())
+                .append("; Date of Birth: ")
+                .append(person.getDateOfBirth())
                 .append("; Tags: ");
         person.getTags().forEach(builder::append);
         return builder.toString();
