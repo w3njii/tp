8ec5ae79836f092fbd931c1ba6e9dafc787ed77f
camlin-tan <187715771+camlin-tan@users.noreply.github.com>
2025-10-08 17:33:26 +0800

    Update style of JsonAdaptedPerson

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -38,7 +38,8 @@ class JsonAdaptedPerson {
     @JsonCreator
     public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
                              @JsonProperty("email") String email, @JsonProperty("address") String address,
-                             @JsonProperty("tags") List<JsonAdaptedTag> tags, @JsonProperty("bloodType") String bloodType) {
+                             @JsonProperty("tags") List<JsonAdaptedTag> tags,
+                             @JsonProperty("bloodType") String bloodType) {
         this.name = name;
         this.phone = phone;
         this.email = email;
